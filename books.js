93a8f11648ecc08de7620963571f0dd2f17556c7
fakepeppino538-a11y <fakepeppino538@gmail.com>
2026-05-27 21:53:50 +0700

    Update books.js

--- books.js
+++ books.js
@@ -13,5 +13,20 @@ const books = [
     title: "The Synagogue of Satan",
     pdf: "books/The_Synagogue_of_Satan.pdf",
     cover: "covers/shopping.jpg"
+  },
+  {
+  title: "The great replacement",
+  pdf: "books/956356442-The-Great-Replacement-Brenton-Tarrant.pdf",
+  cover: "covers/b.jpg"
+  },
+  {
+  title: "Payton manifesto",
+  pdf: "books/PG-Manifesto.pdf",
+  cover: "covers/r.jpg"
+  },
+  {
+  title: "A European Declaration of Independence",
+  pdf: "books/anders-behring-breivik-manifesto_compress.pdf",
+  cover: "covers/a.jpg"
   }
 ];
