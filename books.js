10b0068b6200e396ffa3647951df66936356c676
fakepeppino538-a11y <fakepeppino538@gmail.com>
2026-05-27 22:21:35 +0700

    Update books.js

--- books.js
+++ books.js
@@ -1,18 +1,18 @@
 const books = [
   {
-    title: "TM 21210",
+    title: "TM 21-210 (Improvised Munitions Handbook)",
     pdf: "books/TM_21210.pdf",
     cover: "covers/download.jpg"
   },
   {
     title: "The Anarchist Cookbook",
     pdf: "books/The_Anarchist_Cookbook.pdf",
-    cover: "covers/download2.jpg"
+    cover: "covers/shopping.jpg"
   },
   {
     title: "The Synagogue of Satan",
     pdf: "books/The_Synagogue_of_Satan.pdf",
-    cover: "covers/shopping.jpg"
+    cover: "covers/download2.jpg"
   },
   {
   title: "The great replacement",
