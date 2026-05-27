9d95408eea2bfe312e6cc04b267f6f9e8d14a3bd
fakepeppino538-a11y <fakepeppino538@gmail.com>
2026-05-27 21:04:45 +0700

    Create books.js

---
+++ books.js
@@ -0,0 +1,17 @@
+const books = [
+  {
+    title: "TM 21210",
+    pdf: "books/TM_21210.pdf",
+    cover: "covers/download.jpg"
+  },
+  {
+    title: "The Anarchist Cookbook",
+    pdf: "books/The_Anarchist_Cookbook.pdf",
+    cover: "covers/download2.jpg"
+  },
+  {
+    title: "The Synagogue of Satan",
+    pdf: "books/The_Synagogue_of_Satan.pdf",
+    cover: "covers/shopping.jpg"
+  }
+];
